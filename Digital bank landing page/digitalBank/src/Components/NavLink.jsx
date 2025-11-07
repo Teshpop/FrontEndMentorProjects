199820fbbb2b0eb4184a58435a00ab20415c7081
Pedro Tello <Tello.3810@outlook.com>
2025-11-06 22:06:19 -0600

    Hero text

--- Digital bank landing page/digitalBank/src/Components/NavLink.jsx
+++ Digital bank landing page/digitalBank/src/Components/NavLink.jsx
@@ -5,7 +5,7 @@ const NavLink = ({name}) =>
         <li
             className="group text-gray-600 text-[18px] h-full flex flex-col items-center justify-between"
         >
-            <a href="#" className="flex-1 flex items-center justify-center group-hover:text-black transition-colors duration-200">
+            <a href="#" className="flex-1 flex items-center justify-center group-hover:text-blue-900 transition-colors duration-200">
                 {name}
             </a>
             <div className="bg-green-500 w-[100%] h-[4px] scale-x-0 group-hover:scale-x-100 transition-all duration-200" />
--- Digital bank landing page/digitalBank/src/Pages/Hero.jsx
+++ Digital bank landing page/digitalBank/src/Pages/Hero.jsx
@@ -1,20 +1,27 @@
+import {ButtonRequest} from "../Components"
+
 const Hero = () =>
 {
     return(
         <div className="h-[645px] w-screen bg-gray-50 flex flex-row">
             {/*Text*/}
-            <div className="bg-green-500 h-full w-[40%]">
-                <h1>
-                    Next generation digital banking
-                </h1>
-                <p>
-                    Take your financial life online. Your Digitalbank account will be
-                    a one-stop-shop for spending, saving, budgeting, investing, and much more.
-                </p>
+            <div className="h-full w-[45%] flex justify-center items-center">
+                <div className="flex flex-col justify-center items-start w-[68%] gap-8">
+                    <h1 className="text-blue-900 text-5xl">
+                        Next generation digital banking
+                    </h1>
+
+                    <p className="text-gray-600 text-[18px]">
+                        Take your financial life online. Your Digitalbank account will be
+                        a one-stop-shop for spending, saving, budgeting, investing, and much more.
+                    </p>
+
+                    <ButtonRequest />
+                </div>
             </div>
 
             {/*images*/}
-            <div className="bg-cyan-400 h-full w-[60%] relative">
+            {/*<div className="bg-cyan-400 h-full w-[55%] relative">
                 <img
                     src="/images/bg-intro-desktop.svg" alt="intro desktop"
                     className="absolute top-[-34%] right-[-30%]"
@@ -24,7 +31,7 @@ const Hero = () =>
                     src="/images/image-mockups.png" alt="mockups"
                     className="absolute top-[-21%] right-[-11%] z-0"
                 />
-            </div>
+            </div>*/}
         </div>
     )
 }
